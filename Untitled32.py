a839aef7904cb20e54cf12088b9e8fd858a7746c
AmbatiJayaLakshmi <71976269+AmbatiJayaLakshmi@users.noreply.github.com>
2020-09-27 20:42:37 +0530

    Add files via upload

---
+++ Untitled32.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+#import libraries
+import pandas as pd
+import numpy as np
+#print a line
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
